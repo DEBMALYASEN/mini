f91472a6a38ddebaf43c1a5ac288d0fdc40ce575
DEBMALYASEN <boonysen@gmail.com>
2022-04-14 22:27:51 +0530

    changed import statements

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,8 +3,9 @@ package calculator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
 
